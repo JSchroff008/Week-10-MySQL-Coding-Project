3593167ac5efa24bce45d65157eb73060c294eea
JSchroff008 <jakeschroff@gmail.com>
2022-09-13 17:14:53 -0500

    week 10 java mysql project

---
+++ mysql-java/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,29 @@
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+
+public class ProjectService {
+
+	private ProjectDao projectDao = new ProjectDao(); 
+	/**
+	 * This method calls the DAO class to insert a roj
+	 * 
+	 * @param prject the {@link project} object.
+	 * @return The Project object with the new key value 
+	 * 
+	 */
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);
+	}
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+	}
+	public Project fetchProjectById(Integer projectId) {
+		return projectDao.fetchProjectById(projectId).orElseThrow(() -> 
+		new NoSuchElementException("Project with project ID=" + projectId + " does not exist"));
+	}
+}
